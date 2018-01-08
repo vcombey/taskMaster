ba6699a89810f5560f8014d10bf9643d30193771
ade-sede <adrien.de.sede@gmail.com>
2018-01-08 20:16:18 +0100

    Implementing a few things

--- src/service.rs
+++ src/service.rs
@@ -1,11 +1,27 @@
 use process::Process;
 use std::collections::HashMap;
 pub struct Service {
-    name: String,
-    process: HashMap<String, Process>,
+    /// A service is a list of process bundled together. This allows
+    /// us to call operations on a group of process rather than
+    /// process 1 by 1.
+    list: Hashmap<String, HashMap<String, Process>>
 }
 
 impl Service {
-    pub fn new(name: String) {
+    pub fn new(name: String) -> Service {
+        /// Initialize a new service with name 'name' and no process.
+        Service {
+            list: HashMap::new().insert(name, HashMap::new()),
+        }
+    }
+
+    pub fn get(&self, key: &str) -> Option<&Hashmap<String, Process>> {
+        	self.list.get(key)
+    }
+
+    pub fn get_process(&self, key: &str) -> Option<&Process>
+    pub fn push_process(&mut self, name: String, process: Process)
+    {
+        self.process.insert(name, process)
     }
 }
---
+++ target/doc/implementors/core/str/trait.FromStr.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["serde_derive_internals"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"serde_derive_internals/attr/enum.RenameRule.html\" title=\"enum serde_derive_internals::attr::RenameRule\">RenameRule</a>",];
+implementors["serde_json"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"serde_json/value/enum.Value.html\" title=\"enum serde_json::value::Value\">Value</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
