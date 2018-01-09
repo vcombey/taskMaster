72c3a616a51484d2da6f1301daa83dcfb0437c7d
ade-sede <adrien.de.sede@gmail.com>
2018-01-09 10:41:36 +0100

    Many more structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 extern crate yaml_rust;
-pub mod process;
-pub mod service;
+
+pub mod task_master {
+    use std::collections::HashMap;
+    pub mod process;
+    pub mod service;
+
+    Struct task_master {
+        config_file: String,
+        service_list: HashMap<String, service::Service>,
+    }
+}
--- src/service.rs
+++ src/service.rs
@@ -1,27 +1,23 @@
 use process::Process;
 use std::collections::HashMap;
-pub struct Service {
+enum Service {
     /// A service is a list of process bundled together. This allows
     /// us to call operations on a group of process rather than
     /// process 1 by 1.
-    list: Hashmap<String, HashMap<String, Process>>
+    name: String,
+    process_list: HashMap<String, Process>,
 }
 
 impl Service {
-    pub fn new(name: String) -> Service {
         /// Initialize a new service with name 'name' and no process.
+    pub fn new(name: String) -> Service {
         Service {
-            list: HashMap::new().insert(name, HashMap::new()),
+            name,
+            process_list: HashMap::new();
         }
     }
 
-    pub fn get(&self, key: &str) -> Option<&Hashmap<String, Process>> {
+    pub fn get(&self, key: &str) -> Option<&HashMap<String, Process>> {
         	self.list.get(key)
-    }
-
-    pub fn get_process(&self, key: &str) -> Option<&Process>
-    pub fn push_process(&mut self, name: String, process: Process)
-    {
-        self.process.insert(name, process)
     }
 }
