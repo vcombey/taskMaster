bc4c0b7377c6f9b43e1d813443c86ec3026ffbd8
Vincent COMBEY <vcombey@e3r7p4.42.fr>
2018-01-11 17:40:08 +0100

    hash config mit en methode

--- src/tm_mod/mod.rs
+++ src/tm_mod/mod.rs
@@ -51,7 +51,68 @@ impl<'tm> TmStruct<'tm> {
         for (service, map) in map.into_iter() {
             let mut s = Service::new(service);
             s.launch_from_hash(map);
-           	self.service_hash.insert(s.name.clone(), s);
+            self.service_hash.insert(s.name.clone(), s);
         }
+    }
+
+    pub fn hash_config(&self) -> HashMap<String, HashMap<String,Config>> {
+        /// Reads the config file using TmStruct methods, and turns it
+        /// into a HashMap representing the structure of the services and
+        /// programm we need to launch. Multiple service cannot have the
+        /// same name, and multiple process cannot have the same name EVEN
+        /// ACROSS different services, and finally a process cannot have
+        /// the same name a service does. 0 ambiguity allowed.
+        let doc = task_master.parse_config_file().unwrap();
+        let doc = &doc[0];
+        let doc = doc.as_hash().unwrap();
+
+        /*pk ? pas check little map */
+        let mut taken_process_names: Vec<&str> = Vec::new();
+
+
+        // Big map build
+        let mut big_map = HashMap::new();
+        for (section_name_yaml, section_yaml) in doc.iter() {
+            let section_name = section_name_yaml.as_str().unwrap();
+            let section_hash = section_yaml.as_hash().unwrap();
+
+            // Litle map build
+            let mut little_map = HashMap::new();
+            for (name, config) in section_hash.iter() {
+                match (name.as_str(), config["cmd"].as_str()) {
+                    (Some(name), None) => eprintln!("Missing command for process {}", name),
+                    (None, Some(_)) => eprintln!("Missing process name"),
+                    (None, None) => eprintln!("Missing both process name and command"),
+                    (Some(name), Some(argv)) => {
+
+                        //  Check if a service/process with the same name aready exists
+                        if big_map.contains_key(name) {
+                            eprintln!("Cannot create process of the name '{}': a service of the same name already exists", name);
+                            panic!("Need to improve this server.c");
+                        } else if taken_process_names.contains(&name) {
+                            eprintln!("Cannot create process of the name '{}': a process of the same name already exists", name);
+                            panic!("Need to improve this server.c");
+                        }
+
+                        // Insert into little map
+                        little_map.insert(String::from(name), Config::from_yaml(name, argv, config));
+                        taken_process_names.push(name);
+                    },
+                }
+
+            }
+            // Check if a service / process with the same name already exists
+            //if let Some(_) = big_map.keys().find(|key| { key == &section_name}) {
+            if big_map.contains_key(section_name) {
+                eprintln!("Cannot create service of the name '{}': a service of the same name already exists", section_name);
+                panic!("Need to improve this server.c");
+            } else if taken_process_names.contains(&section_name) {
+                eprintln!("Cannot create service of the name '{}': a process of the same name already exists", section_name);
+                panic!("Need to improve this server.c");
+            }
+            // Insert into big map
+            big_map.insert(String::from(section_name), little_map);
+        }
+        return big_map;
     }
 }
