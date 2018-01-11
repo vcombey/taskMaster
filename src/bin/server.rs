1fb8e427b98c495128eaab8943240cadcdb09178
ade-sede <adrien.de.sede@gmail.com>
2018-01-11 18:00:33 +0100

    Added a variable as allow_unused, removed useless use statement in binary server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,23 +1,6 @@
-#[allow(unused_imports)]
-use std::io;
-#[allow(unused_imports)]
-use std::io::Write;
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)] // REMOVE
-use std::fs::File; // REMOVE
-#[allow(unused_imports)] // REMOVE
-use std::io::prelude::*; // REMOVE
-#[allow(unused_imports)]
-use std::process::Command;
-#[allow(unused_imports)]
-use std::process::Child;
-use std::collections::HashMap;
-
 
 extern crate task_master;
-use task_master::tm_mod::service::thread::process::Process;
-use task_master::tm_mod::config::Config;
 
 fn parse_argv (args: &[String]) -> (&str, &str)
 {
@@ -37,7 +20,9 @@ use task_master::tm_mod::TmStruct;
 fn main()
 {
     let args: Vec<String> = env::args().collect();
-    let (option, filename) = parse_argv(&args);
+
+    // Unused variable right here
+    let (_option, filename) = parse_argv(&args); // UNUSED
     let mut tm = TmStruct::new(filename);
 
     let map = tm.hash_config();
