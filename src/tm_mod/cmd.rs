2c643594a8cdb8ea9fcfcce3635e4c12043489a7
Vincent COMBEY <vcombey@e3r10p17.42.fr>
2018-01-12 18:55:33 +0100

    add cmd

--- src/tm_mod/cmd.rs
+++ src/tm_mod/cmd.rs
@@ -7,3 +7,24 @@ pub enum Cmd {
     STATUS,
     SHUTDOWN,
 }
+
+/// List of possible targets for an instruction.
+/// ALL -> Every single process in every service.
+/// Process(p) -> The process with name p.
+/// Service(s) -> Every single process in service named s.
+/// ServiceProcess(s, p) -> The process name p in service s.
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Target {
+    ALL,
+    Process(String),
+    Service(String),
+    ServiceProcess((String, String)),
+}
+
+/// The struct that will be sent to the server, representing the
+/// operation to launch and the targets to launch it onto.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Cmd {
+    instruction: Instruction,
+    target_vec: Vec<Target>,
+}
