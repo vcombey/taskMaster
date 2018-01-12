17a5de3762cd4da5dfc740bc1cff2428d6e34d27
Vincent COMBEY <vcombey@e3r10p17.42.fr>
2018-01-12 18:09:43 +0100

    client server works but the client reads 512 bytes

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,4 +1,10 @@
+use std::net::{TcpStream,TcpListener};
+use std::io::{Write, Read};
+
 fn main() {
-    /// THis is a comment
-    println!("this is the client");
+  let mut buffer = String::new();
+  let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+  let _ = stream.write(b"lalala");
+    let _ = stream.read_to_string(&mut buffer);
+    println!("message get{}", buffer);
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -16,11 +16,35 @@ fn parse_argv (args: &[String]) -> (&str, &str)
     (option, filename)
 }
 use task_master::tm_mod::TmStruct;
+use std::net::{TcpStream,TcpListener};
+use std::io::{Read, Write};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
+
+fn server(port: &str)
+{
+    let listener = TcpListener::bind(port).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
 
 fn main()
 {
     let args: Vec<String> = env::args().collect();
 
+    server("127.0.0.1:8080");
     // Unused variable right here
     let (_option, filename) = parse_argv(&args); // UNUSED
     let mut tm = TmStruct::new(filename);
